c6b199f114bd4f8556633221e96dff4406404bbc
ranpa <rangelpassos@hotmail.com>
2018-04-13 11:46:08 -0300

    Hello world!

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World, Laddres!');
+});
+
+app.listen(3000, () => {
+  console.log('Laddres is listening on por 3000!');
+})
