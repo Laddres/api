d484226d428a131f977074ef462e613e0a82f5ba
ranpa <rangelpassos@hotmail.com>
2018-05-13 12:55:53 -0300

    chore: utilização do webpack para gerar o build

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-#!/usr/bin/env nodejs
+'use strict'
+
 import dotenv from 'dotenv';
 dotenv.config();
 
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  mode: 'development',
+  target: 'node',
+
+  entry: [
+    path.join(__dirname, '', 'index'),
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'api.node.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+}
