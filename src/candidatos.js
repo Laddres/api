cafeec52f2582b17fccbfc1f4f7cf89f20553dd5
ranpa <rangelpassos@hotmail.com>
2018-05-20 16:19:34 -0300

    feat: novo endpoint para candidatos a partir do id
    Retorna todas as informações sobre um candidato, incluindo todas as
    candidaturas a partir de seu id.

--- src/candidatos.js
+++ src/candidatos.js
@@ -2,7 +2,7 @@
 
 import mysql from 'mysql'
 
-// import candidaturas from './candidaturas'
+import candidaturas from './candidaturas'
 
 const expressaoBusca = termoBusca => (
   termoBusca
@@ -50,8 +50,31 @@ const sqlCompleto = (termoBusca, pagina, itens) => `
   ORDER BY
     candidato.id ASC
   LIMIT ${(pagina - 1) * itens}, ${itens};`
+const sqlExpandido = idCandidato => `
+  SELECT
+    candidato.id,
+    candidato.nome as nome,
+    candidato.data_nascimento,
+    candidato.cpf,
+    candidato.titulo_eleitoral,
+    candidato.email,
+    cidade.nome as cidade_natal,
+    estado.nome as estado_origem,
+    grau_instrucao.descricao as grau_instrucao,
+    ocupacao.descricao as ocupacao,
+    nacionalidade.nome as nacionalidade
+  FROM
+    candidato
+      LEFT JOIN grau_instrucao on candidato.grau_instrucao_id = grau_instrucao.id
+      LEFT JOIN ocupacao on candidato.ocupacao_id = ocupacao.id
+      LEFT JOIN nacionalidade on candidato.nacionalidade_id = nacionalidade.id,
+    cidade
+      INNER JOIN estado on cidade.estado_id = estado.id
+  WHERE
+    candidato.id = ${idCandidato} AND
+    cidade.id = candidato.cidade_id;`
 
-const candidatos = ({
+const candidatosPorNome = ({
   nomeCandidato,
   tipo,
   pagina = 1,
@@ -81,5 +104,45 @@ const candidatos = ({
     })
   })
 )
+
+const candidatosPorId = ({ idCandidato }) => (
+  new Promise((resolve, reject) => {
+    if (!idCandidato) {
+      reject({ statusCode: 400, erro: 'É preciso enviar o nome de um candidato' })
+    }
+
+    const sql = sqlExpandido(parseInt(idCandidato))
+
+    const conn = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    })
+    conn.query(sql, (erro, resultados) => {
+      if (erro) {
+        reject({ statusCode: 500, erro: `Erro inesperado: ${erro}` })
+      }
+
+      if (resultados.length === 0) {
+        resolve([])
+      }
+
+      const candidato = resultados[0]
+      candidaturas({ idCandidato: candidato.id })
+        .then(resultadoCandidaturas => (
+          resolve({ ...candidato, candidaturas: resultadoCandidaturas })
+        ))
+        .catch(erroCandidaturas => (
+          reject({ statusCode: 500, erro: `Erro inesperado: ${erroCandidaturas}` })
+        ))
+    })
+  })
+)
+
+const candidatos = {
+  porNome: candidatosPorNome,
+  porId: candidatosPorId,
+}
 
 export default candidatos
